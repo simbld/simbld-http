b0dbd96762f922bac0101f68aa6988fc439530e3
simbld <s_bullado51@hotmail.com>
2024-11-26 23:52:58 +0100

    Add new interceptors and middleware in example `middleware_usage.rs`, update server binding address.

--- examples/middleware_usage.rs
+++ examples/middleware_usage.rs
@@ -1,15 +1,24 @@
-use actix_web::{middleware, App, HttpServer};
-use simbld_http::helpers::response_middleware_helper::ResponseMiddleware;
+use actix_web::{web, App, HttpServer};
+use simbld_http::helpers::{
+  http_interceptor_helper::HttpInterceptor, http_interceptor_helper2::HttpInterceptor2,
+  response_middleware_helper::ResponseMiddleware, response_middleware_helper2::ResponseMiddleware2,
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   HttpServer::new(|| {
     App::new()
-      .wrap(ResponseMiddleware) // Add middleware
-      .wrap(middleware::Logger::default()) // Optional logging middleware
-      .service(actix_web::web::resource("/").to(|| async { "Hello, Simbld-HTTP!" }))
+      .wrap(ResponseMiddleware) // Adds headers like X-Powered-By
+      .wrap(ResponseMiddleware2)
+      .wrap(HttpInterceptor) // Adds HTTP status descriptions
+      .wrap(HttpInterceptor2)
+      .route("/", web::get().to(example_response))
   })
-  .bind("127.0.0.1:8088")?
+  .bind("127.0.0.1:8080")?
   .run()
   .await
+}
+
+async fn example_response() -> impl actix_web::Responder {
+  simbld_http::responses::server::internal_server_error()
 }
