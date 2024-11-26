3a36cc78cf2f50e341ff13187597f7d88f87bb47
simbld <s_bullado51@hotmail.com>
2024-11-26 23:56:03 +0100

    Add new helpers and re-export HTTP interceptor and response middleware modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+/// Library module exposing helpers and responses
 pub mod helpers;
 pub mod responses;
 
-/// Re-export the responses module
+pub use helpers::http_interceptor_helper::HttpInterceptor;
+pub use helpers::http_interceptor_helper2::HttpInterceptor2;
+/// Re-export core functionalities
 pub use helpers::response_middleware_helper::ResponseMiddleware;
+pub use helpers::response_middleware_helper2::ResponseMiddleware2;
