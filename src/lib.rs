e9dbd2eb12dbd7c25f7afaac4e0d1709fb471012
simbld <s_bullado51@hotmail.com>
2024-11-27 22:33:53 +0100

    Refactor library module to clarify core functionalities and unify middleware imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
-/// Library module exposing helpers and responses
+/// Core library module exposing helpers and responses
 pub mod helpers;
 pub mod responses;
 
+// HTTP Middleware and Interceptors
 pub use helpers::http_interceptor_helper::HttpInterceptor;
-pub use helpers::http_interceptor_helper2::HttpInterceptor2;
-/// Re-export core functionalities
-pub use helpers::response_middleware_helper::ResponseMiddleware;
-pub use helpers::response_middleware_helper2::ResponseMiddleware2;
+pub use helpers::unified_middleware_helper::UnifiedMiddleware;
