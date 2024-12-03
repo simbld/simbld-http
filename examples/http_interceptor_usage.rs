babfa2be2e74ee7a5d1d2d3589784b092ff0df7c
simbld <s_bullado51@hotmail.com>
2024-12-03 23:58:17 +0100

    Refactor HTTP server setup in http_interceptor_usage.rs for improved readability and structure

--- examples/http_interceptor_usage.rs
+++ examples/http_interceptor_usage.rs
@@ -1,15 +1,23 @@
 use actix_web::{web, App, HttpServer, Responder};
 use simbld_http::helpers::http_interceptor_helper::HttpInterceptor;
+use crate::helpers::response_functions::ResponseFunctions;
+use strum::IntoEnumIterator;
+use inflector::Inflector;
+
 
 async fn test_response() -> impl Responder {
-  simbld_http::responses::ResponsesSuccessCodes::Ok()
+  simbld_http::responses::ResponsesSuccessCodes::Ok
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| App::new().wrap(HttpInterceptor).route("/", web::get().to(test_response)))
-    .route("/test", web::get().to(test_response))
-    .bind("127.0.0.1:8087")?
-    .run()
-    .await
+  HttpServer::new(|| {
+    App::new()
+      .wrap(HttpInterceptor)
+      .route("/", web::get().to(test_response))
+      .route("/test", web::get().to(test_response))
+  })
+  .bind("127.0.0.1:8087")?
+  .run()
+  .await
 }
