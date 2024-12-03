df61f5da17942b5b18570b81f8f77286e484642c
simbld <s_bullado51@hotmail.com>
2024-12-03 23:58:46 +0100

    Improve HTTP server configuration in full_usage.rs by adding custom arguments for UnifiedMiddleware and HttpInterceptor middleware

--- examples/full_usage.rs
+++ examples/full_usage.rs
@@ -3,13 +3,21 @@ use simbld_http::helpers::{
   http_interceptor_helper::HttpInterceptor, unified_middleware_helper::UnifiedMiddleware,
 };
 use simbld_http::responses::*;
+use crate::helpers::response_functions::ResponseFunctions;
+use strum::IntoEnumIterator;
+use inflector::Inflector;
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   HttpServer::new(|| {
     App::new()
-      .wrap(UnifiedMiddleware) // Applies a custom middleware
-      .wrap(HttpInterceptor) // Applies another custom middleware
+      .wrap(UnifiedMiddleware::new(
+        vec!["allowed_origins".to_string()],
+        100,
+        std::time::Duration::from_secs(60),
+      )) // Applies a custom middleware
+      .wrap(HttpInterceptor::new("interceptor_arg").expect("Failed to create HttpInterceptor")) // Applies another custom middleware
       .route("/", web::get().to(home)) // Modify here to call the `home` function
       .route("/custom", web::get().to(custom_example))
   })
