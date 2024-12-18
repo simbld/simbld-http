0f4d8dad040d02948db0d0e99279376933cea010
simbld <s_bullado51@hotmail.com>
2024-12-18 23:57:43 +0100

    Add helper functions for JSON responses with custom headers and document their usage

--- src/helpers/response_with_headers_helper.rs
+++ src/helpers/response_with_headers_helper.rs
@@ -1,7 +1,28 @@
 use serde_json::json;
+
 use std::collections::HashMap;
 
 /// This helper function is used to return a JSON response with a status of 200 and a description of "OK" along with the data and headers provided.
+///
+/// # Arguments
+///
+/// * `headers` - A `HashMap` containing the headers to be included in the response.
+///
+/// # Returns
+///
+/// A `String` containing the JSON response.
+///
+/// # Example
+///
+/// ```
+/// use std::collections::HashMap;
+/// use simbld_http::helpers::response_with_headers_helper::ok_with_headers;
+///
+/// let mut headers = HashMap::new();
+/// headers.insert("Content-Type", "application/json");
+/// let response = ok_with_headers(headers);
+/// println!("{}", response);
+/// ```
 pub fn ok_with_headers(headers: HashMap<&str, &str>) -> String {
   json!({
       "status": "OK",
@@ -12,6 +33,27 @@ pub fn ok_with_headers(headers: HashMap<&str, &str>) -> String {
   .to_string()
 }
 
+/// This helper function is used to return a JSON response with a status of 400 and a description of "Bad Request" along with the data and headers provided.
+///
+/// # Arguments
+///
+/// * `headers` - A `HashMap` containing the headers to be included in the response.
+///
+/// # Returns
+///
+/// A `String` containing the JSON response.
+///
+/// # Example
+///
+/// ```
+/// use std::collections::HashMap;
+/// use simbld_http::helpers::response_with_headers_helper::bad_request_with_headers;
+///
+/// let mut headers = HashMap::new();
+/// headers.insert("Content-Type", "application/json");
+/// let response = bad_request_with_headers(headers);
+/// println!("{}", response);
+/// ```
 pub fn bad_request_with_headers(headers: HashMap<&str, &str>) -> String {
   json!({
       "status": "Bad Request",
