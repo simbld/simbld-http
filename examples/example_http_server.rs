88b40358db3eeeae2cc2fad1a99d748bf4fc5799
simbld <s_bullado51@hotmail.com>
2024-12-28 23:56:35 +0100

    Refactor: update HTTP response routes to return structured JSON with metadata and improve clarity of response data

--- examples/example_http_server.rs
+++ examples/example_http_server.rs
@@ -1,29 +1,38 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use simbld_http::responses::client::{bad_request, unauthorized};
-use simbld_http::responses::success::{created, ok};
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde_json::json;
+use simbld_http::helpers::response_helpers::{
+  transform_to_json_with_metadata, transform_to_xml_with_metadata,
+};
+use simbld_http::responses::client::bad_request_tuple;
+use simbld_http::responses::ResponsesTypes;
+use simbld_http::responses::{ResponsesClientCodes, ResponsesSuccessCodes};
 
-/// Route for the 200 OK response.
-async fn ok_route() -> impl Responder {
-  let (code, description) = ok(); // Appel de la fonction générée dynamiquement
-  HttpResponse::build(actix_web::http::StatusCode::from_u16(code).unwrap()).body(description)
+/// Route for the 200 OK XML response with metadata.
+async fn ok_route() -> HttpResponse {
+  let response = ResponsesTypes::Success(ResponsesSuccessCodes::Ok);
+  let xml_str = transform_to_xml_with_metadata(response);
+  HttpResponse::Ok().body(xml_str)
 }
 
 /// Route for the 400 Bad Request response.
-async fn bad_request_route() -> impl Responder {
-  let (code, description) = bad_request();
-  HttpResponse::build(actix_web::http::StatusCode::from_u16(code).unwrap()).body(description)
+async fn bad_request_route() -> HttpResponse {
+  let (code, name, desc) = bad_request_tuple();
+  HttpResponse::BadRequest().json(json!({ "status": code, "name": name, "description": desc }))
 }
 
 /// Route for the 201 Created response.
-async fn created_route() -> impl Responder {
-  let (code, description) = created();
-  HttpResponse::build(actix_web::http::StatusCode::from_u16(code).unwrap()).body(description)
+async fn created_route() -> HttpResponse {
+  let code = ResponsesSuccessCodes::Created;
+  let (code, description) = code.into();
+  HttpResponse::Created()
+    .json(json!({ "status": code, "name": "Created", "description": description }))
 }
 
-/// Route for the 401 Unauthorized response.
-async fn unauthorized_route() -> impl Responder {
-  let (code, description) = unauthorized();
-  HttpResponse::build(actix_web::http::StatusCode::from_u16(code).unwrap()).body(description)
+/// Route for the 401 Unauthorized response JSON with metadata.
+async fn unauthorized_route() -> HttpResponse {
+  let response = ResponsesClientCodes::Unauthorized;
+  let json_str = transform_to_json_with_metadata(ResponsesTypes::ClientError(response));
+  HttpResponse::Unauthorized().json(json_str)
 }
 
 #[actix_web::main]
