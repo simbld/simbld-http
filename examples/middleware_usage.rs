35965c47a28b42384cbeeb96873b926cfec0f8be
simbld <s_bullado51@hotmail.com>
2024-12-31 04:22:11 +0100

    Refactor: simplify example response function and update success code handling

--- examples/middleware_usage.rs
+++ examples/middleware_usage.rs
@@ -1,9 +1,12 @@
 use actix_web::{web, App, HttpServer, Responder};
-use simbld_http::helpers::unified_middleware_helper::UnifiedMiddleware;
-use simbld_http::responses::{CustomResponse, ResponsesServerCodes};
+use simbld_http::helpers::{to_u16_helper::ToU16, unified_middleware_helper::UnifiedMiddleware};
+use simbld_http::responses::{CustomResponse, ResponsesSuccessCodes::Ok};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-use strum::EnumProperty;
+
+async fn example_response() -> impl Responder {
+  CustomResponse::new(Ok.to_u16(), "Success", "Test data", "Request was successful")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -20,14 +23,4 @@ async fn main() -> std::io::Result<()> {
   .bind("127.0.0.1:8090")?
   .run()
   .await
-}
-
-async fn example_response() -> impl Responder {
-  CustomResponse {
-    code: ResponsesServerCodes::InternalServerError.into(),
-    name: ResponsesServerCodes::InternalServerError.get_str("Name").unwrap_or("No name"),
-    description: ResponsesServerCodes::InternalServerError
-      .get_str("Description")
-      .unwrap_or("No description"),
-  }
 }
