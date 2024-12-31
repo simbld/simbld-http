18b476afddd1a136d1a6e25c89e25552ea743c62
simbld <s_bullado51@hotmail.com>
2024-12-31 01:21:13 +0100

    Refactor: update example token middleware to enhance route protection and improve response handling

--- examples/example_token_middleware.rs
+++ examples/example_token_middleware.rs
@@ -1,45 +1,35 @@
-use actix_web::http::StatusCode; // Import for StatusCode
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use simbld_http::responses::client::{invalid_token, page_expired};
-use simbld_http::responses::local::missing_token;
-use simbld_http::responses::success::authentication_successful;
+use actix_web::http::StatusCode;
+/// The above Rust code sets up an HTTP server using Actix-web framework with a route protected by an authentication middleware.
+///
+/// Returns:
+///
+/// The code provided is setting up an Actix Web server that listens on `http://127.0.0.1:8069`. It defines a route `/protected` that is protected by the `AuthMiddleware` middleware. When a GET request is made to `/protected`, it responds with a message "You have a valid token -> Access granted".
+///
+/// http://127.0.0.1:8069/protected?key=validated
+/// http://127.0.0.1:8069/protected?key=expired
+/// http://127.0.0.1:8069/protected?key=mistake
+/// http://127.0.0.1:8069/protected
+///
+use actix_web::{web, App, HttpResponse, HttpServer};
 use simbld_http::AuthMiddleware;
-
-/// Checks if a token is valid or not.
-async fn token_middleware(query: web::Query<TokenParams>) -> impl Responder {
-  let response = match query.key.as_deref() {
-    Some("validated") => {
-      let (code, description) = authentication_successful();
-      HttpResponse::build(StatusCode::from_u16(code).unwrap()).body(description)
-    },
-    Some("expired") => {
-      let (code, description) = page_expired();
-      HttpResponse::build(StatusCode::from_u16(code).unwrap()).body(description)
-    },
-    Some(_) => {
-      let (code, description) = invalid_token();
-      HttpResponse::build(StatusCode::from_u16(code).unwrap()).body(description)
-    },
-    None => {
-      let (code, description) = missing_token();
-      HttpResponse::build(StatusCode::from_u16(code).unwrap()).body(description)
-    },
-  };
-
-  response
-}
-
-#[derive(serde::Deserialize)]
-struct TokenParams {
-  key: Option<String>,
-}
+use std::io;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> io::Result<()> {
   println!("Starting HTTP server at http://127.0.0.1:8069");
 
   HttpServer::new(|| {
-    App::new().wrap(AuthMiddleware).route("/token", web::get().to(token_middleware))
+    App::new()
+      .wrap(AuthMiddleware)
+      // We protect this route via middleware
+      .route(
+        "/protected",
+        web::get().to(|| async {
+          HttpResponse::build(StatusCode::from_u16(200).unwrap())
+            .insert_header(("X-HTTP-Status-Code", "200"))
+            .body("Access Granted")
+        }),
+      )
   })
   .bind("127.0.0.1:8069")?
   .run()
