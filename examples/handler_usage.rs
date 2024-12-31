19080efe83d07a53c134c5121756807aae228d00
simbld <s_bullado51@hotmail.com>
2024-12-31 01:22:33 +0100

    Refactor: update custom response handler to improve success message clarity and provide a description

--- examples/handler_usage.rs
+++ examples/handler_usage.rs
@@ -3,14 +3,14 @@ use simbld_http::helpers::to_u16_helper::ToU16;
 use simbld_http::responses::actix_responder::custom_response_handler;
 use simbld_http::responses::CustomResponse;
 use simbld_http::ResponsesSuccessCodes;
-use strum::EnumProperty;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   let custom_response = CustomResponse::new(
     ResponsesSuccessCodes::Ok.to_u16(),
-    "OK",
-    ResponsesSuccessCodes::Ok.get_str("Description").unwrap_or("No description"),
+    "Success",
+    "Test data",
+    "Request was successful",
   );
 
   HttpServer::new(move || {
