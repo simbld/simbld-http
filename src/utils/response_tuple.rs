e913d72b79c1f6df92a666efcaacf8d67664c504
simbld <s_bullado51@hotmail.com>
2025-01-06 23:57:17 +0100

    Refactor: enhance documentation for get_or_default function in response_tuple module

--- src/utils/response_tuple.rs
+++ src/utils/response_tuple.rs
@@ -1,3 +1,17 @@
+/// The function `as_struct` constructs a `ResponseTuple` struct based on certain key-value pairs and default values.
+///
+/// Arguments:
+///
+/// * `key`: The `key` parameter in the `get_or_default` function is a reference to a string that represents the key used to retrieve a value from a data structure. This function attempts to get the value associated with the given key and returns it if it exists. If the value does not exist, it
+/// * `default`: The `default` parameter in the `get_or_default` function is used to provide a default value in case the key is not found in the data structure being accessed. If the key is not found, the function will return the default value provided instead.
+///
+/// Returns:
+///
+/// The `as_struct` function returns a `ResponseTuple` struct based on the data extracted from the input. The function constructs a `ResponseTuple` object with the following fields:
+fn get_or_default(&self, key: &str, default: &'static str) -> &'static str {
+  self.get_str(key).unwrap_or(default)
+}
+
 pub struct ResponseTuple {
   pub std_code: u16,
   pub std_name: &'static str,
@@ -25,8 +39,8 @@ pub fn as_struct(&self) -> ResponseTuple {
     ResponseTuple {
       std_code,
       std_name,
-      int_code,
-      int_name,
+      int_code: Some(int_code),
+      int_name: Some(int_name),
       desc,
     }
   }
