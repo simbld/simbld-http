d382f5118fae94b8278e5775d564b762fca1b83c
simbld <s_bullado51@hotmail.com>
2025-02-27 23:53:41 +0100

    Improvement of the unifieduple structure to uniformly represent the HTTP response codes, adding serialization and an AS_JSON method for the conversion to JSON.

--- src/helpers/unified_tuple_helper.rs
+++ src/helpers/unified_tuple_helper.rs
@@ -1,17 +1,61 @@
-//! This file defines the UnifiedTuple struct.
-//! It represents a response code in a unified way using optional fields.
-//! Example: If standard_code equals internal_code, internal_code and internal_name are None.
+/// This file defines the UnifiedTuple struct used to represent HTTP response codes uniformly.
+/// Example:
+/// ```rust
+/// use simbld_http::helpers::unified_tuple_helper::UnifiedTuple;
+/// let tuple = UnifiedTuple {
+///     standard_code: 200,
+///     standard_name: "OK",
+///     unified_description: "Successful request",
+///     internal_code: None,
+///     internal_name: None,
+/// };
+/// assert_eq!(tuple.as_json()["description"], "Successful request");
+/// ```
+use crate::responses::ResponsesTypes;
+use serde::Serialize;
 
-#[derive(Debug, PartialEq)]
+
+#[derive(Debug, PartialEq, Serialize)]
 pub struct UnifiedTuple {
-  /// Standard HTTP code.
-  pub code: u16,
-  /// Standard HTTP name.
-  pub name: &'static str,
-  /// Description of the response.
-  pub description: &'static str,
-  /// Internal HTTP code (None if equal to standard code).
-  pub internal_code: Option<u16>,
-  /// Internal HTTP name (None if equal to standard name).
-  pub internal_name: Option<&'static str>,
+    /// Standard HTTP code.
+    pub standard_code: u16,
+    /// Standard HTTP name.
+    pub standard_name: &'static str,
+    /// Description of the response.
+    pub unified_description: &'static str,
+    /// Internal HTTP code (None if equal to standard code).
+    pub internal_code: Option<u16>,
+    /// Internal HTTP name (None if equal to standard name).
+    pub internal_name: Option<&'static str>,
+}
+
+impl UnifiedTuple {
+    pub fn as_json(&self) -> serde_json::Value {
+        serde_json::json!({
+            "standard http code": {
+                "code": self.standard_code,
+                "name": self.standard_name,
+            },
+            "description": self.unified_description,
+            "internal http code": {
+                "code": self.internal_code,
+                "name": self.internal_name,
+            },
+        })
+    }
+}
+
+impl From<ResponsesTypes> for UnifiedTuple {
+    fn from(response: ResponsesTypes) -> Self {
+        let standard_code = response.to_u16();
+        let standard_name = response.description();
+        let unified_description = response.description();
+        UnifiedTuple {
+            standard_code,
+            standard_name,
+            unified_description,
+            internal_code: None,
+            internal_name: None,
+        }
+    }
 }
