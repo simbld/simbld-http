d93d2bc9d3a380a7082db8abc5788bfcd6829dc8
simbld <s_bullado51@hotmail.com>
2025-03-04 23:55:32 +0100

    Add a TwofieldStuple structure to simplify the representation of HTTP codes with two fields. Implementation of conversion from httpcode.

---
+++ src/helpers/two_fields_tuple_helper.rs
@@ -0,0 +1,23 @@
+use crate::helpers::http_code_helper::HttpCode;
+use crate::helpers::tuple_traits::IntoTwoFieldsTuple;
+use serde::Serialize;
+
+/// SimpleTuple represents a simplified view of HttpCode with two fields.
+#[derive(Debug, Serialize)]
+pub struct TwoFieldsTuple {
+    pub code: u16,
+    pub name: &'static str,
+}
+
+impl TwoFieldsTuple {
+    /// Create a new TwoFieldsTuple from a HttpCode.
+    pub fn from_http_code(http_code: &HttpCode) -> Self {
+        Self { code: http_code.standard_code, name: http_code.standard_name }
+    }
+}
+
+impl IntoTwoFieldsTuple for HttpCode {
+    fn into_two_fields_tuple(self) -> TwoFieldsTuple {
+        TwoFieldsTuple::from_http_code(&self)
+    }
+}
