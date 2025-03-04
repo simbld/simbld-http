2fdd433f286e84b8eadd2ace4f219703a6c0c3ba
simbld <s_bullado51@hotmail.com>
2025-03-04 23:51:12 +0100

    Refactor handler_usage.rs for improved code formatting and consistency

--- examples/handler_usage.rs
+++ examples/handler_usage.rs
@@ -1,24 +1,24 @@
 use actix_web::{web, App, HttpServer};
-use simbld_http::helpers::to_u16_helper::ToU16;
+use simbld_http::helpers::to_u16_trait::ToU16;
 use simbld_http::responses::actix_responder::custom_response_handler;
 use simbld_http::responses::CustomResponse;
 use simbld_http::ResponsesSuccessCodes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  let custom_response = CustomResponse::new(
-    ResponsesSuccessCodes::Ok.to_u16(),
-    "Success",
-    "Test data",
-    "Request was successful",
-  );
+    let custom_response = CustomResponse::new(
+        ResponsesSuccessCodes::Ok.to_u16(),
+        "Success",
+        "Test data",
+        "Request was successful",
+    );
 
-  HttpServer::new(move || {
-    App::new()
-      .app_data(web::Data::new(custom_response.clone()))
-      .route("/", web::get().to(custom_response_handler))
-  })
-  .bind("127.0.0.1:8088")?
-  .run()
-  .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(custom_response.clone()))
+            .route("/", web::get().to(custom_response_handler))
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
