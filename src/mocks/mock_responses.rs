9fdaec5cebd253edad076831b6edf03b1a1f8fc2
simbld <s_bullado51@hotmail.com>
2025-04-14 06:59:11 +0200

    Refactor implementation of `From<MockResponses>` for clarity and consistency

--- src/mocks/mock_responses.rs
+++ src/mocks/mock_responses.rs
@@ -2,34 +2,34 @@ use std::fmt;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum MockResponses {
-  Ok,
-  BadRequest,
-  Unauthorized,
-  NotFound,
-  InternalServerError,
+    Ok,
+    BadRequest,
+    Unauthorized,
+    NotFound,
+    InternalServerError,
 }
 
-impl Into<u16> for MockResponses {
-  fn into(self) -> u16 {
-    match self {
-      MockResponses::Ok => 200,
-      MockResponses::BadRequest => 400,
-      MockResponses::Unauthorized => 401,
-      MockResponses::NotFound => 404,
-      MockResponses::InternalServerError => 500,
+impl From<MockResponses> for u16 {
+    fn from(val: MockResponses) -> Self {
+        match val {
+            MockResponses::Ok => 200,
+            MockResponses::BadRequest => 400,
+            MockResponses::Unauthorized => 401,
+            MockResponses::NotFound => 404,
+            MockResponses::InternalServerError => 500,
+        }
     }
-  }
 }
 
 impl fmt::Display for MockResponses {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    let response_name = match self {
-      MockResponses::Ok => "Ok",
-      MockResponses::BadRequest => "BadRequest",
-      MockResponses::Unauthorized => "Unauthorized",
-      MockResponses::NotFound => "NotFound",
-      MockResponses::InternalServerError => "InternalServerError",
-    };
-    write!(f, "{}", response_name)
-  }
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let response_name = match self {
+            MockResponses::Ok => "Ok",
+            MockResponses::BadRequest => "BadRequest",
+            MockResponses::Unauthorized => "Unauthorized",
+            MockResponses::NotFound => "NotFound",
+            MockResponses::InternalServerError => "InternalServerError",
+        };
+        write!(f, "{}", response_name)
+    }
 }
